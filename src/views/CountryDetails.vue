bff3f366f2d65d2ecbaeb8351683e67d0ba398db
lauracapella <laura@laguapacomunicacion.com>
2022-08-15 18:16:19 +0200

    iteration 1.4

--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -1,6 +1,16 @@
 <template>
   <h1>hola</h1>
   <div v-for="(country, index) in countries" :key="index">
+   <!--  <h1> codigo {{country.alpha3Code}}</h1>
+    <h1> codigo params {{$route.params.details}}</h1> -->
+
+
+    <div v-if="country.alpha3Code === `${$route.params.details}`">
+     {{country.name.common}}
+     {{country.capital}}
+     {{country.area}}
+     {{country.borders}}
+    </div>
 
       <!-- Usando una ruta estatica! -->
       <!-- <router-link :to="country.alpha3Code">
@@ -37,6 +47,8 @@ export default {
     console.log("routes:", this.$route.params.details)
     console.log("code:", countries.alpha3Code)
     this.details =  this.$route.params.details 
+    const codigoPais = this.details
+    return codigoPais;
     /* const route = useRoute();
     console.warn("route", route.params); */
   },
